d2cbf2046fb45ca07f8bc95ffa6a467f92eb217b
Alben Bustamante <albensnico@gmail.com>
2021-11-18 18:25:24 -0500

    Base construída para futuras ventanas.

--- src/main/java/com/danicode/gui/model/GUIWindow.java
+++ src/main/java/com/danicode/gui/model/GUIWindow.java
@@ -1,4 +1,26 @@
 package com.danicode.gui.model;
 
-public class GUIWindow {
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public abstract class GUIWindow extends JFrame {
+    public GUIWindow(int width, int height, String title, boolean resizable) {
+        super(title);
+        this.setSize(width, height);
+        this.setLocationRelativeTo(null);
+        this.setResizable(resizable);
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent ev) { GUIWindow.this.close(); }
+        });
+        this.init();
+        this.setVisible(true);
+    }
+
+    protected abstract void close();
+
+    protected abstract void init();
 }
