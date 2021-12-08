64c4343428a576446fbb45a2170395d36bd2b0f6
Alben Bustamante <albensnico@gmail.com>
2021-12-08 17:03:59 -0500

    Definidas sentencias SQL básicas para usuarios.

---
+++ src/main/java/com/danicode/microblogging/dao/implementations/DAOUserImpl.java
@@ -0,0 +1,35 @@
+package com.danicode.microblogging.dao.implementations;
+
+import com.danicode.microblogging.dao.templates.DAOUser;
+import com.danicode.microblogging.domain.User;
+
+import java.util.List;
+
+public class DAOUserImpl implements DAOUser {
+    private static final String
+        SQL_INSERT = "INSERT INTO users(name, last_name, email, username, password) VALUES(?, ?, ?, ?, ?);",
+        SQL_SELECT = "SELECT * FROM users;",
+        SQL_UPDATE = "UPDATE users SET name=?, last_name=?, email=?, username=?, password=? WHERE user_id=?;",
+        SQL_DELETE = "DELETE FROM users WHERE user_id=?;",
+        SQL_SELECT_BY_ID = "SELECT name, last_name, email, username, password FROM users WHERE user_id=?;";
+
+    @Override
+    public int create(User element) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<User> list() throws Exception {
+        return null;
+    }
+
+    @Override
+    public int update(User element) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int delete(int idElement) throws Exception {
+        return 0;
+    }
+}
