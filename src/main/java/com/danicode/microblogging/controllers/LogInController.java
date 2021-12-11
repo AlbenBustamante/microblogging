aeee6134948d94e055ce870d634f98e61adadfe9
Alben Bustamante <albensnico@gmail.com>
2021-12-10 20:27:59 -0500

    Establecidas las acciones a los componentes.

--- src/main/java/com/danicode/microblogging/controllers/LogInController.java
+++ src/main/java/com/danicode/microblogging/controllers/LogInController.java
@@ -5,6 +5,8 @@ import com.danicode.microblogging.gui.login.LogInTemplate;
 import com.danicode.microblogging.gui.login.SignUpTemplate;
 import com.danicode.microblogging.services.UserService;
 
+import javax.swing.*;
+
 public class LogInController {
     private GUILogIn logIn;
     private LogInTemplate logInTemplate;
@@ -16,5 +18,26 @@ public class LogInController {
         this.logInTemplate = this.logIn.getLogInTemplate();
         this.signUpTemplate = this.logIn.getSignUpTemplate();
         this.userService = new UserService();
+        this.setActions();
+    }
+
+    private void logIn() {
+        JOptionPane.showMessageDialog(null, "INICIANDO SESIÓN...");
+    }
+
+    private void signUp() {
+        JOptionPane.showMessageDialog(null, "REGISTRANDO...");
+    }
+
+    private void setActions() {
+        this.logInTemplate.getbLogIn().addActionListener(e -> this.logIn());
+        this.logInTemplate.getJUser().addActionListener(e -> this.logIn());
+        this.logInTemplate.getJPassword().addActionListener(e -> this.logIn());
+        this.signUpTemplate.getBSignUp().addActionListener(e -> this.signUp());
+        this.signUpTemplate.getJName().addActionListener(e -> this.signUp());
+        this.signUpTemplate.getJLastName().addActionListener(e -> this.signUp());
+        this.signUpTemplate.getJEmail().addActionListener(e -> this.signUp());
+        this.signUpTemplate.getJUsername().addActionListener(e -> this.signUp());
+        this.signUpTemplate.getJPassword().addActionListener(e -> this.signUp());
     }
 }
