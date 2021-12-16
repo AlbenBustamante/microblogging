660cdb07c3e286e0b6585570186dfd74bdcc42b4
Alben Bustamante <albensnico@gmail.com>
2021-12-15 23:13:36 -0500

    Definidas sentencias SQL.

--- src/main/java/com/danicode/microblogging/model/dao/implementations/DAOMessageImpl.java
+++ src/main/java/com/danicode/microblogging/model/dao/implementations/DAOMessageImpl.java
@@ -10,6 +10,12 @@ import java.util.List;
 public class DAOMessageImpl implements DAOMessage {
     private Connection externConnection;
     private DAOUser userDao;
+    private static final String
+        SQL_INSERT = "INSERT INTO messages(user_id_pk, date_time, message) VALUES(?, ?, ?);",
+        SQL_SELECT = "SELECT message_id, user_id_pk, date_time, message FROM messages;",
+        SQL_UPDATE = "UPDATE messages SET message = ? WHERE message_id = ?;",
+        SQL_DELETE = "DELETE FROM messages WHERE message_id = ?,",
+        SQL_SELECT_BY_ID = "SELECT user_id_pk, date_time, message FROM messages WHERE message_id = ?;";
 
     public DAOMessageImpl() {
         this.userDao = new DAOUserImpl();
