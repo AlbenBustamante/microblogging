1f1a1ed53e281e27c1c4654963f988499f216da2
Alben Bustamante <albensnico@gmail.com>
2021-12-18 16:57:19 -0500

    Definida interfaz gráfica para perfil de usuario.

--- src/main/java/com/danicode/microblogging/gui/users/GUIViewProfile.java
+++ src/main/java/com/danicode/microblogging/gui/users/GUIViewProfile.java
@@ -1,31 +1,26 @@
 package com.danicode.microblogging.gui.users;
 
 import com.danicode.microblogging.gui.model.GUIDialog;
+import com.danicode.microblogging.gui.model.layouts.IGridBagLayout;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GUIViewProfile extends GUIDialog {
-    private String name, lastName, email, username;
-    private int userId;
     private JLabel lUserId, lFullName, lEmail, lUsername;
     private JButton bExit;
     private JPanel centerPane, southPane;
+    private IGridBagLayout gbc;
 
-    public GUIViewProfile(JFrame owner, int userId, String name, String lastName, String email, String username) {
-        super(owner, 460, 500, "Perfil de Usuario", false, true, new BorderLayout());
-        this.userId = userId;
-        this.name = name;
-        this.lastName = lastName;
-        this.email = email;
-        this.username = username;
+    public GUIViewProfile(JFrame owner) {
+        super(owner, 474, 304, "Perfil de Usuario", false, false, new BorderLayout());
     }
 
     private void createLabels() {
-        this.lUserId = new JLabel("Identificador de Usuario: " + this.userId);
-        this.lFullName = new JLabel("Nombre Completo: " + this.name + " " + this.lastName);
-        this.lEmail = new JLabel("Email: " + this.email);
-        this.lUsername = new JLabel("Nombre de Usuario: @" + this.username);
+        this.lUserId = new JLabel();
+        this.lFullName = new JLabel();
+        this.lEmail = new JLabel();
+        this.lUsername = new JLabel();
     }
 
     private void createButtons() {
@@ -38,6 +33,30 @@ public class GUIViewProfile extends GUIDialog {
         this.centerPane.setLayout(new GridBagLayout());
         this.southPane.setLayout(new FlowLayout(FlowLayout.RIGHT, 14, 10));
         this.southPane.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.GRAY));
+        this.gbc = () -> this.centerPane;
+    }
+
+    private void design() {
+        final int WIDTH = 3;
+
+        this.gbc.addSpaces(0, WIDTH);
+
+        this.gbc.addGBC(1, 1, 1, 1, 1.0, 0, GridBagConstraints.HORIZONTAL, this.lUserId);
+        this.gbc.addFinalSpaces(WIDTH, 1);
+
+        this.gbc.addGBC(1, 3, 1, 1, 1.0, 0, GridBagConstraints.HORIZONTAL, this.lFullName);
+        this.gbc.addFinalSpaces(WIDTH, 3);
+
+        this.gbc.addGBC(1, 5, 1, 1, 1.0, 0, GridBagConstraints.HORIZONTAL, this.lEmail);
+        this.gbc.addFinalSpaces(WIDTH, 5);
+
+        this.gbc.addGBC(1, 7, 1, 1, 1.0, 0, GridBagConstraints.HORIZONTAL, this.lUsername);
+        this.gbc.addFinalSpaces(WIDTH, 7);
+
+        this.southPane.add(this.bExit);
+
+        this.getContentPane().add(this.gbc.pane(), BorderLayout.CENTER);
+        this.getContentPane().add(this.southPane, BorderLayout.SOUTH);
     }
 
     @Override
@@ -50,5 +69,16 @@ public class GUIViewProfile extends GUIDialog {
         this.createPanels();
         this.createLabels();
         this.createButtons();
+        this.design();
     }
+
+    public JLabel getLUserId() { return this.lUserId; }
+
+    public JLabel getLFullName() { return this.lFullName; }
+
+    public JLabel getLEmail() { return this.lEmail; }
+
+    public JLabel getLUsername() { return this.lUsername; }
+
+    public JButton getBExit() { return this.bExit; }
 }
