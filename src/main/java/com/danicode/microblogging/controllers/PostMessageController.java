d8476d1cfc0d1beb4949f10f8ed5f4a88539d54a
Alben Bustamante <albensnico@gmail.com>
2021-12-17 22:00:18 -0500

    Definido método para mostrar caracteres restantes.

--- src/main/java/com/danicode/microblogging/controllers/PostMessageController.java
+++ src/main/java/com/danicode/microblogging/controllers/PostMessageController.java
@@ -6,6 +6,7 @@ import com.danicode.microblogging.gui.messages.GUIPostMessage;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import java.awt.*;
 
 public class PostMessageController implements DocumentListener {
     private GUIPostMessage postMessage;
@@ -24,14 +25,34 @@ public class PostMessageController implements DocumentListener {
         this.setActions();
     }
 
+    private void checkCharacters() {
+        var length = this.postMessage.getJPost().getText().strip().length();
+        var remaining = 0;
+        var color = Color.BLACK;
+        var text = "";
+
+        if (length > 140) {
+            remaining = - (this.postMessage.POST_LENGTH - length) + 1;
+            text = "Límite excedido: " + --remaining;
+            color = Color.RED;
+        }
+        else {
+            remaining = this.postMessage.POST_LENGTH - length;
+            text = this.postMessage.CHARACTERS_REMAINING + remaining;
+        }
+
+        this.postMessage.getLCharacters().setText(text);
+        this.postMessage.getLCharacters().setForeground(color);
+    }
+
     @Override
     public void insertUpdate(DocumentEvent documentEvent) {
-        this.postMessage.getLCharacters().setText("HOLAAA");
+        this.checkCharacters();
     }
 
     @Override
     public void removeUpdate(DocumentEvent documentEvent) {
-        this.postMessage.getLCharacters().setText("CHAAAAO");
+        this.checkCharacters();
     }
 
     @Override
