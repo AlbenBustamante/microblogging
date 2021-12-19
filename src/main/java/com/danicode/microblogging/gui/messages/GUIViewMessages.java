4a9a791fb125f85b4ac39b0504ae65986f81e3b1
Alben Bustamante <albensnico@gmail.com>
2021-12-18 19:05:19 -0500

    Diseñado buscador.

---
+++ src/main/java/com/danicode/microblogging/gui/messages/GUIViewMessages.java
@@ -0,0 +1,60 @@
+package com.danicode.microblogging.gui.messages;
+
+import com.danicode.microblogging.gui.model.GUIDialog;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIViewMessages extends GUIDialog {
+    private JPanel northPane;
+    private JTextField tfSearch;
+    private JComboBox<String> cbSearchType;
+    private JButton bSearch;
+
+    public GUIViewMessages(JFrame owner) {
+        super(owner, 560, 700, "Microblogging", false, false, new BorderLayout());
+    }
+
+    private void createPanels() {
+        this.northPane = new JPanel();
+        this.northPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 10));
+        this.northPane.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
+    }
+
+    private void createFields() {
+        this.tfSearch = new JTextField("Buscador", 20);
+    }
+
+    private void createComboBoxes() {
+        this.cbSearchType = new JComboBox<>(new String[] {
+                "Mensajes", "Por Usuario"
+        });
+    }
+
+    private void createButtons() {
+        this.bSearch = new JButton("Buscar");
+    }
+
+    private void design() {
+        /* North Pane */
+        this.northPane.add(this.tfSearch);
+        this.northPane.add(this.cbSearchType);
+        this.northPane.add(this.bSearch);
+
+        this.getContentPane().add(this.northPane, BorderLayout.NORTH);
+    }
+
+    @Override
+    protected void close() {
+        this.dispose();
+    }
+
+    @Override
+    protected void init() {
+        this.createPanels();
+        this.createFields();
+        this.createComboBoxes();
+        this.createButtons();
+        this.design();
+    }
+}
