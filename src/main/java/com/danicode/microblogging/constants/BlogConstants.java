0ba5cb667f87a16bf4314b89afb6bb9cb29a092b
Alben Bustamante <albensnico@gmail.com>
2021-12-19 11:24:57 -0500

    Agregadas nuevas constantes para filtrar mensajes.

--- src/main/java/com/danicode/microblogging/constants/BlogConstants.java
+++ src/main/java/com/danicode/microblogging/constants/BlogConstants.java
@@ -2,6 +2,9 @@ package com.danicode.microblogging.constants;
 
 public interface BlogConstants {
     int POST_LENGTH = 140;
+    int LIST_MESSAGES = 0;
+    int LIST_USER_MESSAGES = 1;
+    int LIST_BY_MESSAGE = 2;
     String CHARACTERS_REMAINING = "Caracteres restantes: ";
     String SEARCH_MESSAGES = "Todos los Mensajes";
     String SEARCH_BY_MESSAGE = "Por mensaje";
