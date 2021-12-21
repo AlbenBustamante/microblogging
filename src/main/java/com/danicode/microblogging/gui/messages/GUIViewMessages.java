766f868b9c2b7a3b4583af0b67351e7920b4f3b0
Alben Bustamante <albensnico@gmail.com>
2021-12-21 16:53:27 -0500

    Importadas las constantes y aplicadas en el combobox de filtro.

--- src/main/java/com/danicode/microblogging/gui/messages/GUIViewMessages.java
+++ src/main/java/com/danicode/microblogging/gui/messages/GUIViewMessages.java
@@ -1,5 +1,6 @@
 package com.danicode.microblogging.gui.messages;
 
+import com.danicode.microblogging.constants.BlogConstants;
 import com.danicode.microblogging.gui.model.GUIDialog;
 
 import javax.swing.*;
@@ -38,7 +39,8 @@ public class GUIViewMessages extends GUIDialog {
 
     private void createComboBoxes() {
         this.cbSearchType = new JComboBox<>(new String[] {
-                "Mensajes", "Por Usuario"
+                BlogConstants.SEARCH_MESSAGES, BlogConstants.SEARCH_BY_MESSAGE, BlogConstants.SEARCH_MESSAGE_BY_USER,
+                BlogConstants.SEARCH_BY_USER_LOGGED
         });
     }
 
