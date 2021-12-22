0a4e0d11a282d36cacdd556791618dc64438b9b6
Alben Bustamante <albensnico@gmail.com>
2021-12-22 03:24:31 -0500

    Añadido el punto de entrada de la aplicación.

---
+++ src/main/java/com/danicode/microblogging/App.java
@@ -0,0 +1,11 @@
+package com.danicode.microblogging;
+
+import com.danicode.microblogging.controllers.LogInController;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String... alnicode32) {
+        SwingUtilities.invokeLater(LogInController::new);
+    }
+}
