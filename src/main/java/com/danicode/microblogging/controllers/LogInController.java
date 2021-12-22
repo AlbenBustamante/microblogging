4ff5835174541b03ed21d2bb6deb013a8e395fda
Alben Bustamante <albensnico@gmail.com>
2021-12-22 03:20:28 -0500

    LogInController Pequeña modularización y JavaDocs

--- src/main/java/com/danicode/microblogging/controllers/LogInController.java
+++ src/main/java/com/danicode/microblogging/controllers/LogInController.java
@@ -10,14 +10,24 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 
+/**
+ * Esta clase se encarga de gestionar a los usuarios, es decir, establecer un usuario logueado y/o registrar un nuevo usuario.
+ * */
 public class LogInController {
-    private GUILogIn logIn;
-    private LogInTemplate logInTemplate;
-    private SignUpTemplate signUpTemplate;
-    private UserService userService;
+    private final GUILogIn logIn;
+    private final LogInTemplate logInTemplate;
+    private final SignUpTemplate signUpTemplate;
+    private final UserService userService;
     private User user;
     private String logUsername, logPassword, signName, signLastName, signEmail, signUsername, signPassword;
 
+    /**
+     * No recibe parámetros, simplemente se encarga de crear las instancias necesarias.
+     * @see com.danicode.microblogging.gui.login.GUILogIn
+     * @see com.danicode.microblogging.gui.login.LogInTemplate
+     * @see com.danicode.microblogging.gui.login.SignUpTemplate
+     * @see com.danicode.microblogging.services.UserService
+     * */
     public LogInController() {
         this.logIn = new GUILogIn();
         this.logInTemplate = this.logIn.getLogInTemplate();
@@ -27,31 +37,50 @@ public class LogInController {
         this.setActions();
     }
 
+    /**
+     * Obtiene los datos de los campos de texto para iniciar sesión y los guarda en su respectiva variable.
+     * */
     private void setLogInData() {
         this.logUsername = this.logInTemplate.getJUser().getText().strip();
         this.logPassword = new String(this.logInTemplate.getJPassword().getPassword()).strip();
     }
 
+    /**
+     * Establece los datos mediante {@code setLogInData}
+     * @return Devuelve verdadero si todos o algún dato está vacío.
+     * */
     private boolean isLogInEmpty() {
         this.setLogInData();
-
         return this.logUsername.equals("") || this.logPassword.equals("");
     }
 
+    /**
+     * Hace una consulta a la base de datos y establece al usuario logueado.
+     * @return Devuelve verdadero si no ocurrió ningún error.
+     * */
     private boolean setUserLogged() {
         this.user = new User("", "", this.logUsername, this.logUsername, this.logPassword);
-
         return this.userService.setUserLogged(this.user);
     }
 
+    /**
+     * Salimos de la ventana para iniciar sesión y vamos al menú principal del programa.
+     * */
+    private void run() {
+        var username = this.userService.getUserLogged().getUsername();
+        JOptionPane.showMessageDialog(null, "¡Bienvenido " + username + "!",
+                "Sesión abierta", JOptionPane.INFORMATION_MESSAGE);
+        new MainMenuController();
+        this.logIn.dispose();
+    }
+
+    /**
+     * Realiza varias validaciones y va al menú principal si los datos están bien introducidos.
+     * */
     private void logIn() {
         if (!this.isLogInEmpty()) {
             if (this.setUserLogged()) {
-                JOptionPane.showMessageDialog(null,
-                        "¡Bienvenido " + this.userService.getUserLogged().getUsername() + "!",
-                        "Sesión abierta", JOptionPane.INFORMATION_MESSAGE);
-                new MainMenuController();
-                this.logIn.dispose();
+                this.run();
             }
             else {
                 JOptionPane.showMessageDialog(null, "Verifica los datos nuevamente",
@@ -64,6 +93,9 @@ public class LogInController {
         }
     }
 
+    /**
+     * Obtiene los datos de los campos de texto para registrar un nuevo usuario y los guarda en su respectiva variable.
+     * */
     private void setSignUpData() {
         this.signName = this.signUpTemplate.getJName().getText().strip();
         this.signLastName = this.signUpTemplate.getJLastName().getText().strip();
@@ -72,6 +104,10 @@ public class LogInController {
         this.signPassword = new String(this.signUpTemplate.getJPassword().getPassword()).strip();
     }
 
+    /**
+     * Establece los datos mediante {@code setSignUpData}
+     * @return Devuelve verdadero si todos o algún campo está vacío.
+     * */
     private boolean isSignUpEmpty() {
         this.setSignUpData();
 
@@ -79,22 +115,25 @@ public class LogInController {
                 this.signUsername.equals("") || this.signPassword.equals("");
     }
 
+    /**
+     * Consulta a la base de datos si pudo registrar un nuevo usuario
+     * @return Devuelve verdadero si no hubo ningún error
+     * */
     private boolean newUserRegistered() {
         this.user = new User(this.signName, this.signLastName, this.signEmail, this.signUsername, this.signPassword);
-
         return this.userService.registerNewUser(this.user);
     }
 
+    /**
+     * Simplemente muestra un mensaje dependiendo si se logró registrar el usuario.
+     * */
     private void signUp() {
         if (!this.isSignUpEmpty()) {
-            if (this.newUserRegistered()) {
-                JOptionPane.showMessageDialog(null, "¡Usuario registrado con éxito!",
-                        "Nuevo usuario: " + this.user.getUsername(), JOptionPane.INFORMATION_MESSAGE);
-            } else {
-                JOptionPane.showMessageDialog(null,
-                        "El nombre de usuario y/o correo electrónico ya se encuentra registrado.",
-                        "No fue posible registrar un nuevo usuario", JOptionPane.INFORMATION_MESSAGE);
-            }
+            var message = this.newUserRegistered() ? "¡Usuario registrado con éxito!" :
+                    "El nombre de usuario y/o correo electrónico ya se encuentra registrado";
+            var title = this.newUserRegistered() ? "Nuevo usuario: " + this.user.getUsername() :
+                    "No fue posible registrarte";
+            JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
         }
         else {
             JOptionPane.showMessageDialog(null, "Por favor, llena todos los campos para registrarte",
@@ -102,12 +141,18 @@ public class LogInController {
         }
     }
 
+    /**
+     * Establece las acciones a los campos de texto y botones.
+     * */
     private void setActions() {
         this.setActions(e -> this.logIn(), this.logInTemplate.getbLogIn(), this.logInTemplate.getJUser(), this.logInTemplate.getJPassword());
         this.setActions(e -> this.signUp(), this.signUpTemplate.getBSignUp(), this.signUpTemplate.getJName(), this.signUpTemplate.getJLastName(),
                 this.signUpTemplate.getJEmail(), this.signUpTemplate.getJUsername(), this.signUpTemplate.getJPassword());
     }
 
+    /**
+     * Establece la misma acción a varios campos de texto y/o contraseña.
+     * */
     private void setActions(ActionListener event, JButton button, JTextField... fields) {
         Arrays.stream(fields).forEach(field -> field.addActionListener(event));
         button.addActionListener(event);
