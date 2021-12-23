07711448dde22611f72813ff4d452f9ec5d22726
Alben Bustamante <albensnico@gmail.com>
2021-12-22 20:25:44 -0500

    ViewProfileController JavaDocs

--- src/main/java/com/danicode/microblogging/controllers/ViewProfileController.java
+++ src/main/java/com/danicode/microblogging/controllers/ViewProfileController.java
@@ -9,11 +9,20 @@ import com.danicode.microblogging.services.MessageService;
 import java.awt.*;
 import java.util.List;
 
+/**
+ * Permite visualizar el perfil de un usuario.
+ */
 public class ViewProfileController {
     private final GUIViewProfile template;
     private final User user;
     private final List<Message> userMessages;
 
+    /**
+     * Es necesario pasar el usuario por parámetro, para así extraer los datos y visualizarlos.
+     * @param owner Ventana padre
+     * @param user Usuario a visualizar.
+     * @see GUIViewProfile
+     */
     public ViewProfileController(Window owner, User user) {
         this.user = user;
         var message = new Message(user, "");
@@ -23,6 +32,9 @@ public class ViewProfileController {
         this.setLabelTexts();
     }
 
+    /**
+     * Reemplaza el texto de cada label por información del usuario.
+     */
     private void setLabelTexts() {
         this.template.getLUserId().setText("Identificador: #" + this.user.getIdUser());
         this.template.getLFullName().setText("Nombre Completo: " + this.user.getName() + " " + this.user.getLastName());
