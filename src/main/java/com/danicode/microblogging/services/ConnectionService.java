c24eb801110ed252a26f27838f0d73d9ebec521a
Alben Bustamante <albensnico@gmail.com>
2021-12-22 20:40:28 -0500

    ConnectionService JavaDocs

--- src/main/java/com/danicode/microblogging/services/ConnectionService.java
+++ src/main/java/com/danicode/microblogging/services/ConnectionService.java
@@ -7,43 +7,79 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+/**
+ * Se encarga de gestionar la conexión a base de datos MySQL.
+ */
 public class ConnectionService {
     private static final String URL = "jdbc:mysql://localhost:3306/microblogging";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "admin";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final int INITIAL_SIZE = 50;
     private static BasicDataSource dataSource = null;
 
+    /**
+     * Crea un nuevo {@code DataSource} y establece la configuración sólo si es {@code null}.
+     * <p>Es necesario para crear un pool de conexiones.</p>
+     * @return Devuelve el {@code DataSource} y su configuración.
+     */
     private static DataSource getDataSource() {
         if (dataSource == null) {
             dataSource = new BasicDataSource();
             dataSource.setUrl(URL);
             dataSource.setUsername(USERNAME);
             dataSource.setPassword(PASSWORD);
-            dataSource.setInitialSize(50);
+            dataSource.setInitialSize(INITIAL_SIZE);
         }
         return dataSource;
     }
 
+    /**
+     * Establece el driver de MySQL y crea una nueva conexión.
+     * @return Devuelve la nueva conexión.
+     * @throws Exception Puede ser ClassNotFoundException y/o SQLException.
+     */
     public static Connection getConnection() throws Exception {
         Class.forName(DRIVER);
         return getDataSource().getConnection();
     }
 
+    /**
+     * Cuerra la conexion interna dado caso que no exista la conexion externa.
+     * <p>La conexión externa es para manejar transacciones.</p>
+     * @param externConnection Conexion externa.
+     * @param conn Conexion interna.
+     * @throws Exception Puede ser ClassNotFoundException y/o SQLException.
+     */
     public static void close(Connection externConnection, Connection conn) throws Exception {
         if (externConnection == null) {
             conn.close();
         }
     }
 
-    public static void close(Connection externConnection, Connection conn, PreparedStatement pstmt) throws Exception {
-        pstmt.close();
+    /**
+     * Cierra primero el {@code PreparedStatement} y posteriormente la conexion.
+     * @param externConnection Conexión externa.
+     * @param conn Conexión interna.
+     * @param stmt Sentencia preparada.
+     * @throws Exception Puede ser ClassNotFoundException y/o SQLException.
+     */
+    public static void close(Connection externConnection, Connection conn, PreparedStatement stmt) throws Exception {
+        stmt.close();
         close(externConnection, conn);
     }
 
-    public static void close(Connection externConnection, Connection conn, PreparedStatement pstmt,
+    /**
+     * Cierra primero el conjunto de resultados, luego la sentencia preparada y posteriormente, la conexión.
+     * @param externConnection Conexión externa.
+     * @param conn Conexión interna.
+     * @param stmt Sentencia Preparada.
+     * @param rs Conjunto de resultados.
+     * @throws Exception Puede ser ClassNotFoundException y/o SQLException.
+     */
+    public static void close(Connection externConnection, Connection conn, PreparedStatement stmt,
                              ResultSet rs) throws Exception {
         rs.close();
-        close(externConnection, conn, pstmt);
+        close(externConnection, conn, stmt);
     }
 }
